f97a9291231bf1e38ed1e745aa69f9bac6596f4b
Linus <bhattaraisunil76@gmail.com>
2026-07-25 20:14:22 +0545

    edit lab script

--- scripts/run-all-labs.sh
+++ scripts/run-all-labs.sh
@@ -2,4 +2,4 @@ for i in {1..17}; do
 (
     cd "../Lab-$i" && echo -e "\e[33m?\e[37mmain \e[32m~/B.Sc. CSIT/6th_sem/lab reports/SunilNCCLab/Lab-$i> dotnet \e[37mrun\e[0m" && dotnet run
 )
-done
+done | tee all-outputs.txt
