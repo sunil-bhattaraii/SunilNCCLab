2999cb9f12ca37f5cbc2f6e142e5130316eb030a
Linus <bhattaraisunil76@gmail.com>
2026-08-27 18:01:18 +0545

    l-21 init

---
+++ part-2/WebApp4BySunil-Lab-21/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApp4BySunil_Lab_21.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
