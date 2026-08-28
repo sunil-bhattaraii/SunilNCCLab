63982586fc06c900bfad1b074bad612f642a1c93
Linus <bhattaraisunil76@gmail.com>
2026-08-28 19:48:03 +0545

    init and scaffold

---
+++ part-2/WebApp7BySunil-Lab-25/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApp7BySunil_Lab_25.Models;
+
+namespace WebApp7BySunil_Lab_25.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ part-2/WebApp7BySunil-Lab-25/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApp7BySunil_Lab_25;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Faculty { get; set; } = null!;
+}
