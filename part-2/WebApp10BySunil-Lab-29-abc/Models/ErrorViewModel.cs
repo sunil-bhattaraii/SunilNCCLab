48cbbae5904a64334b61f698d75a198c74995ba9
Linus <bhattaraisunil76@gmail.com>
2026-08-29 12:23:17 +0545

    remove useless init

--- part-2/WebApp10BySunil-Lab-29-abc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApp10BySunil_Lab_29_abc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
