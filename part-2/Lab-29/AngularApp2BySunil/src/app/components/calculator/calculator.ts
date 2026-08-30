a26eeea936dccf455d33084bb7ad323e0bc53e44
Linus <bhattaraisunil76@gmail.com>
2026-08-30 13:48:30 +0545

    l29 d done

--- part-2/Lab-29/AngularApp2BySunil/src/app/components/calculator/calculator.ts
+++ part-2/Lab-29/AngularApp2BySunil/src/app/components/calculator/calculator.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculator.html',
   styleUrl: './calculator.css',
 })
-export class Calculator {}
+export class Calculator {
+  a: number;
+  b: number;
+  op: string;
+  result: number;
+
+  constructor() {
+    this.a = 0;
+    this.b = 0;
+    this.op = '+';
+    this.result = 0;
+  }
+
+  handleSubmit() {
+    this.result = eval(this.a + this.op + this.b);
+  }
+}
